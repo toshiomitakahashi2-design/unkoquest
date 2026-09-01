ae2b1827fdcfce70a7e7aca49c4115a6f437cdc7
toshiomitakahashi2 <toshiomi.takahashi2@gmail.com>
2026-09-02 01:08:21 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    ・HTML と JSON は「ネットワークさきに みる」＝更新が すぐ反映される
    ・画像などは キャッシュさきに みる＝はやい
    ・オフラインでも あそべる                                        */
-const CACHE = 'unkoq-v5';
+const CACHE = 'unkoq-v6';
 const ASSETS = [
   './',
   './index.html',
